fbcb2879a765d0f4179ae5642aea33293ec17311
larvata <alzzlnewtype@gmail.com>
2016-09-13 22:03:06 +0800

    fix: wrong title

--- src/server/routes.js
+++ src/server/routes.js
@@ -9,10 +9,17 @@ import store from '../memoryStore';
 import { miichan, auth, schedule } from './api';
 const router = express.Router();
 
-const ClientRoutes = [
-  '/',
-  '/manage',
+const definedRoutes = [
+  {
+    path: '/',
+    title: '48系番组直播间',
+  },
+  {
+    path: '/manage',
+    title: '番组表编辑',
+  },
 ];
+
 
 // handle restful apis
 router.get('/api/miichan/:guid/:currentVersion', miichan);
@@ -46,15 +53,16 @@ router.get('*', (req, res, next) => {
 
   // console.log('sessionId:', req.sessionID);
   // console.log('test');
-  const isClientRoute = ClientRoutes.includes(req.originalUrl.toLowerCase());
-  if (!isClientRoute) {
+  const matchedRoute = definedRoutes.find(r => r.path === req.originalUrl.toLowerCase());
+  // const isClientRoute = ClientRoutes.includes(req.originalUrl.toLowerCase());
+  if (!matchedRoute) {
     return next();
   }
 
   const storeData = store.getAll();
   const storeDataString = JSON.stringify(storeData);
   const authenticated = req.session.user.authenticated;
-  return res.render('index', { title: 'Main', store: storeDataString, authenticated });
+  return res.render('index', { title: matchedRoute.title, store: storeDataString, authenticated });
 });
 
 
